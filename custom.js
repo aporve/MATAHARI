bab80b401587f2cd0eaea0b3b1cc9afc404c7931
aporve <aporvep@gmail.com>
2021-07-19 19:30:20 +0530

    Initial commit

---
+++ custom.js
@@ -0,0 +1,18 @@
+(function injectJS() {
+    try {        
+        var parentCssHead = document.head || document.getElementsByTagName('head')[0];
+        var parentStyles = document.createElement('style');
+        parentStyles.type = 'text/css';
+
+        var parentCssStyles = '@media only screen and (max-width: 768px) { #ymDivCircle { bottom: 75px; } @media only screen and (min-width: 768px) { #ymDivCircle { bottom: 20px; } }';
+        if (parentStyles.styleSheet) {
+        parentStyles.styleSheet.cssText = parentCssStyles;
+        } else {
+        parentStyles.appendChild(document.createTextNode(parentCssStyles));
+        }
+        parentCssHead.appendChild(parentStyles);
+    } catch(e) {
+        console.error("failed while inserting to iFrame", e);
+    }
+})();
+
