c69c4acc7818df5956bbeee8d55438da4698e9cf
aporve <60807797+aporve@users.noreply.github.com>
2022-10-12 18:07:04 +0530

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,18 +1,23 @@
-(function injectJS() {
-    try {        
-        var parentCssHead = document.head || document.getElementsByTagName('head')[0];
-        var parentStyles = document.createElement('style');
-        parentStyles.type = 'text/css';
+// (function injectJS() {
+//     try {        
+//         var parentCssHead = document.head || document.getElementsByTagName('head')[0];
+//         var parentStyles = document.createElement('style');
+//         parentStyles.type = 'text/css';
 
-        var parentCssStyles = '@media only screen and (max-width: 768px) { #ymDivCircle { bottom: 75px; } @media only screen and (min-width: 768px) { #ymDivCircle { bottom: 20px; } }';
-        if (parentStyles.styleSheet) {
-        parentStyles.styleSheet.cssText = parentCssStyles;
-        } else {
-        parentStyles.appendChild(document.createTextNode(parentCssStyles));
-        }
-        parentCssHead.appendChild(parentStyles);
-    } catch(e) {
-        console.error("failed while inserting to iFrame", e);
-    }
-})();
+//         var parentCssStyles = '@media only screen and (max-width: 768px) { #ymDivCircle { bottom: 75px; } @media only screen and (min-width: 768px) { #ymDivCircle { bottom: 20px; } }';
+//         if (parentStyles.styleSheet) {
+//         parentStyles.styleSheet.cssText = parentCssStyles;
+//         } else {
+//         parentStyles.appendChild(document.createTextNode(parentCssStyles));
+//         }
+//         parentCssHead.appendChild(parentStyles);
+//     } catch(e) {
+//         console.error("failed while inserting to iFrame", e);
+//     }
+// })();
+
+document.getElementById('ymDivCircle').style.bottom = '128px !important';
+var observer = new MutationObserver(function(mutations) {
+    document.getElementById('ym-notification').style.bottom = '128px !important'; 
+});
 
